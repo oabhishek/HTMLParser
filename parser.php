4ef97c83ae251a39c91d698b275a9f1915b6d65f
oabhishek <37676016+oabhishek@users.noreply.github.com>
2022-06-15 16:53:35 -0700

    Adding config

--- parser.php
+++ parser.php
@@ -2,7 +2,12 @@
 
 class HTMLParser {
 
-    public $h_limit = 6;
+    public $h_max, $h_char;
+
+    public function __construct($config) {
+        $this->h_max = $config['header_max'];
+        $this->h_char = $config['header_char'];
+    }
 
     public function parse($text) {
         $text = $this->parseHeadersOrPara($text);
@@ -16,9 +21,7 @@ class HTMLParser {
             $paragraph = explode(" ", trim($value));
             $first_word = str_split($paragraph[0]);
             if ($this->isHeader($first_word)) {
-                //echo "\nthis is a header";
                 unset($paragraph[0]);
-                //var_dump($paragraph);
                 $h_count = count($first_word);
                 $h_title = implode(" ", $paragraph);
                 // I assumed this sanitization logic
@@ -26,8 +29,8 @@ class HTMLParser {
                     $data[$key] = "<h$h_count>$h_title</h$h_count>";
                 }
             } elseif (trim($value) !== "") {
-                    // Based on the current requirement I decided to combine both 
-                    $data[$key] = "<p>$value</p>";
+                // Based on the current requirement I decided to combine both 
+                $data[$key] = "<p>$value</p>";
             }
         }
     
@@ -39,14 +42,12 @@ class HTMLParser {
     public function isHeader($word_arr) {
         // each character of this array has to be '#'
         foreach ($word_arr as $char) {
-            if ($char !== "#") {
+            if ($char !== $this->h_char) {
                 return false;
             }
         }
-
-        // echo "\n this->h_limit = ".$this->h_limit." \n";
-        // echo "\n count($word_arr) = ".count($word_arr)." \n";
-        if (count($word_arr) > $this->h_limit) {
+        // Allowed header tags only
+        if (count($word_arr) > $this->h_max) {
             return false;
         }
         return true;
@@ -57,9 +58,5 @@ class HTMLParser {
         return $result;
     }
 }
-
-
-
-
 
 ?>
