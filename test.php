1b9c3eb1d129e3936c6c4dd6387cd2e94ece5234
oabhishek <37676016+oabhishek@users.noreply.github.com>
2022-06-15 16:54:44 -0700

    Adding config

--- test.php
+++ test.php
@@ -1,15 +1,31 @@
 <?php
 require_once 'parser.php';
 
-$text = "###### Sample Document
+
+// Test 1
+$text1 = "###### Sample Document
 Hello!
 
 This is sample markdown ### for the [Mailchimp ](  https://www.mailchimp.com) homework assignment.
 ";
 
 
-// parse h1 and p
+// Test 2
+$text2 = "# Header one
+Hello there
+How are you?
+What's going on?
+## Another Header
+This is a paragraph [with an inline link](http://google.com). Neat, eh?
+## This is a header [with a link](http://yahoo.com)";
 
-$parser = new HTMLParser();
-print_r($parser->parse($text));
+$config = array(
+    "header_max"=> 6,
+    "header_char"=> "#"
+);
+$parser = new HTMLParser($config);
+
+print_r($parser->parse($text1));
+print_r($parser->parse($text2));
+
 ?>
